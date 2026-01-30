a9d7355728522bc2487ba836eddcb6bd6d58c8d8
ajaysharma-7 <ajaysharmaggc97@gmail.com>
2026-01-30 16:31:02 +0530

    update code

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
-const createDomainValidator = require('./middleware/domainValidator');
 const userRouter = require("./router/userRouter")
 const mongoose = require("mongoose")
 
 app.set('trust proxy', true);
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
-const domainValidator = createDomainValidator({
-    allowed: ['awasthiorbitcareer.com'],
-    action: 'redirect',               // 'block' | 'redirect' | 'close'
-    redirectTo: 'https://awasthiorbitcareer.com', // used only for redirect
-    allowWww: true,
-    allowSubdomains: false,
-    logger: (msg, level) => console.log(`[DOMAIN-${level}]`, msg),
-});
-app.use(domainValidator);
+
 app.use(userRouter)
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
